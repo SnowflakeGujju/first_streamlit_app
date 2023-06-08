5b87b54cbf4361cffbf5f8d06b92d2a422161579
SnowflakeGujju <135892196+SnowflakeGujju@users.noreply.github.com>
2023-06-08 16:46:39 -0400

    Update streamlit_app.py
    Nope, two way to select didn't work.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Add pick list for users to pick the items
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
+
 # Display the table
 streamlit.dataframe(my_fruit_list)
