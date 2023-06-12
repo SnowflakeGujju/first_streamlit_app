995d249e4069bdd5cd8bf0e57284391f97f39d6d
SnowflakeGujju <135892196+SnowflakeGujju@users.noreply.github.com>
2023-06-12 10:20:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 
 # take the json response and normalize it - parse the json elements using pandas refering back to the alias we created for pandas
